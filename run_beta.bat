e1325073d36eb5d02092951a755bc98ab98d26a4
Cosk <54825470+Coskon@users.noreply.github.com>
2024-05-23 13:03:17 -0300

    Add files via upload

---
+++ run_beta.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+SET "VENV_NAME=venv"
+
+IF EXIST "%VENV_NAME%\Scripts\activate.bat" (
+  ECHO venv already exists, running...
+) ELSE (
+  ECHO Creating venv...
+  python.exe -m venv "%VENV_NAME%"
+  ECHO venv created, installing requirements...
+)
+
+CALL "%VENV_NAME%\Scripts\activate"
+pip install -r requirements_beta.txt
+pip install --upgrade youtube-dl
+
+python.exe lang\lang.py
+python.exe bot.py
+
+PAUSE
